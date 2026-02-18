2a0cac23d2b433b197d071c8fb366869e6fb1980
josemanuel96 <josemvicente22@gmail.com>
2026-02-18 12:07:33 -0400

    feat: entrada de usuario y validación con Tryparse

---
+++ 01-csharp-fundamentos/CrudEmpleadosConsola/Program.cs
@@ -0,0 +1,27 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("=== Sistema de Empleados ===");
+        Console.Write("Ingrese su nombre: ");
+        string nombre = Console.ReadLine()!;
+
+        Console.Write("Ingrese edad: ");
+        string edadTexto = Console.ReadLine()!;
+
+        int edad;
+
+        if (int.TryParse(edadTexto, out edad))
+        {
+            Console.WriteLine($"Hola {nombre}, tienes {edad} años.");
+        }
+        else
+        {
+            Console.WriteLine("Edad inválida. Debe de ingresar un número.");
+        }
+
+        Console.WriteLine("Presione una tecla para salir...");
+        Console.ReadKey();
+
+    }
+}
