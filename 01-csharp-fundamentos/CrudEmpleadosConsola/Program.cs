dec41f217f792e2a1a1170d0ab3ac7057a075ad4
josemanuel96 <josemvicente22@gmail.com>
2026-02-20 11:52:33 -0400

    feat: estructura base de menu con while y switch

--- 01-csharp-fundamentos/CrudEmpleadosConsola/Program.cs
+++ 01-csharp-fundamentos/CrudEmpleadosConsola/Program.cs
@@ -2,26 +2,62 @@
 {
     private static void Main(string[] args)
     {
-        Console.WriteLine("=== Sistema de Empleados ===");
-        Console.Write("Ingrese su nombre: ");
-        string nombre = Console.ReadLine()!;
+        bool salir = false;
 
-        Console.Write("Ingrese edad: ");
-        string edadTexto = Console.ReadLine()!;
-
-        int edad;
-
-        if (int.TryParse(edadTexto, out edad))
+        while (!salir)
         {
-            Console.WriteLine($"Hola {nombre}, tienes {edad} años.");
-        }
-        else
-        {
-            Console.WriteLine("Edad inválida. Debe de ingresar un número.");
-        }
+            Console.Clear();
+            Console.WriteLine("=== SISTEMA DE EMPLEADOS ===");
+            Console.WriteLine("1. Crear empleado");
+            Console.WriteLine("2. Listar empleado");
+            Console.WriteLine("3. Buscar empleado");
+            Console.WriteLine("4. Editar empleado");
+            Console.WriteLine("5. Eliminar empleado");
+            Console.WriteLine("6. Salir");
+            Console.Write("\nSeleccione una opcion: ");
 
-        Console.WriteLine("Presione una tecla para salir...");
-        Console.ReadKey();
+            string opcion = Console.ReadLine()!;
+
+            switch (opcion)
+            {
+                case "1":
+                    Console.WriteLine("Opcion Crear seleccionada");
+                    break;
+
+                case "2":
+                    Console.WriteLine("Opcion Listar seleccionada");
+                    break;
+
+                case "3":
+                    Console.WriteLine("Opcion Buscar seleccionada");
+                    break;
+
+                case "4":
+                    Console.WriteLine("Opcion Editar seleccionada");
+                    break;
+
+                case "5":
+                    Console.WriteLine("Opcion Eliminar seleccionada");
+                    break;
+
+                case "6":
+                    salir = true;
+                    Console.WriteLine("Saliendo del sistema...");
+                    break;
+
+                default:
+                    Console.WriteLine("Opcion invalida.");
+                    break;
+
+            }
+
+            if (!salir)
+            {
+                Console.WriteLine("\nPresione una tecla para continuar...");
+                Console.ReadKey();
+            }
+
+        }
 
     }
 }
